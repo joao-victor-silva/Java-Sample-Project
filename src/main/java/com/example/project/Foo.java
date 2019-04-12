0aed64e5ec03f30c728be08e64145c819f2129e3
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2019-04-12 11:19:30 -0300

    Added the saySomething() method in Foo class

--- src/main/java/com/example/project/Foo.java
+++ src/main/java/com/example/project/Foo.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
 public class Foo {
-	public String sayHello() {
-		return "Hello";
-	}
+  public String sayHello() {
+    return "Hello";
+  }
+
+  public String saySomething() {
+    return "The class name is Foo";
+  }
 }
